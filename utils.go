b3ba059ffaa2b808f658cd57a1d48e04710daa1b
Ivan Sorokin <redbronze@mail.ru>
2023-09-08 18:34:34 +0300

    init commit

---
+++ utils.go
@@ -0,0 +1,18 @@
+package utils
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
